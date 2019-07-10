bc402822dd4ab0f1a5112986a62463e4ab68eb93
chrissnyder <c.snyder@m1finance.com>
2019-07-10 13:09:32 -0500

    Add some convenience methods

--- src/index.js
+++ src/index.js
@@ -5,7 +5,26 @@ import toString from 'lodash/toString';
 
 import { Sender } from './sender';
 
+const UTM_SOURCE = ('utm_source': 'utm_source');
+const UTM_MEDIUM = ('utm_medium': 'utm_medium');
+const UTM_CAMPAIGN = ('utm_campaign': 'utm_campaign');
+const UTM_TERM = ('utm_term': 'utm_term');
+const UTM_CONTENT = ('utm_content': 'utm_content');
+
+const SOURCE = ('source': 'source');
+const MEDIUM = ('medium': 'medium');
+const CAMPAIGN = ('campaign': 'campaign');
+const TERM = ('term': 'term');
+const CONTENT = ('content': 'content');
+
+type Attribution = {
+  [SOURCE | MEDIUM | CAMPAIGN | TERM | CONTENT]: string
+};
+
 export default class CdsSender {
+  static REQUIRED_TAGS = [SOURCE, MEDIUM, CAMPAIGN];
+  static ALL_TAGS = [...CdsSender.REQUIRED_TAGS, TERM, CONTENT];
+
   _sender: Sender;
 
   constructor(url: string) {
@@ -19,14 +38,43 @@ export default class CdsSender {
   recordAttribution(location: Location) {
     const search = qs.parse(location.search);
     const attribution = readAttribution(search);
-    if (attribution) {
-      const setters = [];
-      for (const [tag, value] of Object.entries(attribution)) {
-        setters.push(this._sender.set(tag, value));
-      }
-      return Promise.all(setters);
+    return attribution
+      ? this.setAttribution(attribution)
+      : Promise.resolve(null);
+  }
+
+  setAttribution(attribution: Attribution) {
+    if (
+      !attribution[SOURCE] ||
+      !attribution[MEDIUM] ||
+      !attribution[CAMPAIGN]
+    ) {
+      return Promise.resolve(null);
     }
-    return Promise.resolve(null);
+    const setters = [];
+    for (const [tag, value] of Object.entries(attribution)) {
+      setters.push(this._sender.set(tag, value));
+    }
+    return Promise.all(setters);
+  }
+
+  hasAttribution(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this._sender.get(...CdsSender.REQUIRED_TAGS)
+        .then(result => resolve(result.filter(Boolean).length === CdsSender.REQUIRED_TAGS.length))
+        .catch((e) => reject(e));
+    });
+  }
+
+  isReferredByOrganicSearch(): boolean {
+    switch (document.referrer) {
+      case 'https://www.google.com':
+      case 'https://www.bing.com':
+      case 'https://www.yahoo.com':
+        return true;
+      default:
+        return false;
+    }
   }
 
   get(): Promise<string> {
@@ -46,20 +94,20 @@ export default class CdsSender {
   }
 }
 
-function readAttribution(search) {
+function readAttribution(search): Attribution {
   if (
-    !search['utm_source'] ||
-    !search['utm_medium'] ||
-    !search['utm_campaign']
+    !search[UTM_SOURCE] ||
+    !search[UTM_MEDIUM] ||
+    !search[UTM_CAMPAIGN]
   ) {
     return null;
   }
 
   return {
-    source: toString(search['utm_source']),
-    medium: toString(search['utm_medium']),
-    campaign: toString(search['utm_campaign']),
-    term: toString(search['utm_term']) || null,
-    content: toString(search['utm_content']) || null
+    SOURCE: toString(search[UTM_SOURCE]),
+    MEDIUM: toString(search[UTM_MEDIUM]),
+    CAMPAIGN: toString(search[UTM_CAMPAIGN]),
+    TERM: toString(search[UTM_TERM]) || null,
+    CONTENT: toString(search[UTM_CONTENT]) || null
   };
 }
